55521847ec13caff44ef3b60cfbb6039b585e297
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-12-17 21:22:35 +0000

    init dom

--- src/Main.elm
+++ src/Main.elm
@@ -3,9 +3,42 @@ module Main exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+
+newTodo : Html msg
+newTodo =
+    Html.form []
+        [ div [ class "field has-addons" ]
+            [ div [ class "control is-expanded" ]
+                [ input [ class "input", attribute "type" "text", placeholder "Neuer Eintrag" ] []
+                ]
+            , div [ class "control" ]
+                [ a [ class "button is-danger" ] [ text "Erstellen" ]
+                ]
+            ]
+        ]
+
+
+entry : String -> Html msg
+entry label =
+    li []
+        [ div [ class "card" ]
+            [ div [ class "header" ]
+                [ p [ class "card-header-title" ] [ text label ]
+                ]
+            ]
+        ]
+
+
 main : Html msg
 main =
     div []
-        [ text "hoho"
-        , text "hihi"
+        [ section [ class "section" ]
+            [ newTodo
+            ]
+        , section [ class "section" ]
+            [ ul
+                []
+                [ entry "Test Eintrag"
+                ]
+            ]
         ]
